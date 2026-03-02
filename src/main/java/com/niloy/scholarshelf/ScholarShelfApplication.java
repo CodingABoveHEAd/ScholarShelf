0a3b82e999007e776065b38886391665c91dab85
niloy <chowdhuryniloy819@gmail.com>
2026-03-02 22:40:45 +0600

    first commit

---
+++ src/main/java/com/niloy/scholarshelf/ScholarShelfApplication.java
@@ -0,0 +1,13 @@
+package com.niloy.scholarshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niloy/scholarshelf/ScholarShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niloy.scholarshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
