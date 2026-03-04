e401a5c46e2f5f841101e1e5d8e8604b6451fd74
niloy <chowdhuryniloy819@gmail.com>
2026-03-04 17:14:56 +0600

    fixed a cloudinary dependency issue

--- src/main/java/com/niloy/scholarshelf/config/CloudinaryConfig.java
+++ src/main/java/com/niloy/scholarshelf/config/CloudinaryConfig.java
@@ -1,11 +1,10 @@
 package com.niloy.scholarshelf.config;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
 
 /**
  * Configures and exposes the Cloudinary client bean using credentials
@@ -25,7 +24,7 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(Map.of(
+        return new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", cloudName,
                 "api_key",    apiKey,
                 "api_secret", apiSecret,
