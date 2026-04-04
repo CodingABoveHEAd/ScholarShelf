f9ba9ceb2abc3365b1d42607c18869c5baa1a723
FaribLearnGit <sadidsiddiquev4@gmail.com>
2026-04-04 21:48:31 +0600

    Testing Fully Done

--- src/test/java/com/niloy/scholarshelf/ScholarShelfApplicationTests.java
+++ src/test/java/com/niloy/scholarshelf/ScholarShelfApplicationTests.java
@@ -3,6 +3,10 @@ package com.niloy.scholarshelf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Basic context load test for the ScholarShelf application. This test ensures that the Spring application context can start successfully without any issues.
+ * It serves as a sanity check to verify that the application configuration is correct and that all necessary
+ */
 @SpringBootTest
 class ScholarShelfApplicationTests {
 
